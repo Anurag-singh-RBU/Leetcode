5b29467d90a87adb9e05c89891ce7699f66c14fc
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-12-25 14:36:26 +0530

    Time: 26 ms (5.14%) | Memory: 63.3 MB (5.37%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public static int dp[][];
+
+    public static int solve(int[] satisfaction , int idx , int ptr){
+
+        if(idx == satisfaction.length){
+
+            return 0;
+
+        }
+
+        if(dp[idx][ptr] != -1) return dp[idx][ptr]; 
+            
+        int LTC = ptr * satisfaction[idx];
+        int take = LTC + solve(satisfaction , idx + 1 , ptr + 1); 
+
+        int skip = solve(satisfaction , idx + 1 , ptr);
+
+        return dp[idx][ptr] = Math.max(take , skip);
+
+    }
+
+    public int maxSatisfaction(int[] satisfaction) {
+
+        dp = new int[501][502];
+
+        for(int i = 0 ; i < 501 ; i++){
+
+            Arrays.fill(dp[i] , -1);
+
+        }
+
+        Arrays.sort(satisfaction);
+
+        return solve(satisfaction , 0 , 1);
+        
+    }
+}
