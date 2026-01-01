47f5e5e8ecf375e98fedf2bf5005599841d29bdf
ANURAG SINGH <anuragsingh260805@gmail.com>
2026-01-02 00:04:51 +0530

    Time: 55 ms (39.38%) | Memory: 55 MB (76.17%) - LeetSync

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    private int m;
+    private int k;
+    private final int MOD = (int) 1e9 + 7;
+    private int[][] memo;
+
+    private int solve(int i , int j , long[][] freq , String target){
+
+        if(i == m){
+
+            return 1;
+
+        }
+
+        if(j == k){
+
+            return 0;
+
+        }
+
+        if(memo[i][j] != -1){
+
+            return memo[i][j];
+
+        }
+
+        int notTaken = solve(i , j + 1 , freq , target) % MOD;
+
+        int taken = (int) ((freq[target.charAt(i) - 'a'][j] * solve(i + 1 , j + 1 , freq , target)) % MOD);
+
+        return memo[i][j] = (notTaken + taken) % MOD;
+
+    }
+
+    public int numWays(String[] words, String target) {
+
+        k = words[0].length();
+        m = target.length();
+
+        long[][] freq = new long[26][k];
+
+        for(String word : words){
+
+            for(int col = 0 ; col < k ; col++){
+
+                freq[word.charAt(col) - 'a'][col]++;
+
+            }
+
+        }
+
+        memo = new int[m][k];
+
+        for(int[] row : memo){
+
+            Arrays.fill(row , -1);
+
+        }
+
+        return solve(0 , 0 , freq , target);
+
+    }
+}
