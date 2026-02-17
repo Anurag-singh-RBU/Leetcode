0fa0c95d15699f8141a50d5bf695790deacfa7d2
ANURAG SINGH <anuragsingh260805@gmail.com>
2026-02-18 03:07:47 +0530

    Time: 7 ms (36.11%) | Memory: 49.4 MB (32.69%) - LeetSync

---
+++ 401-binary-watch/binary-watch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+
+        List<String> res = new ArrayList<>();
+
+        for(int h = 0; h < 12; h++){
+
+            for(int m = 0; m < 60; m++){
+
+                if(Integer.bitCount(h) + Integer.bitCount(m) == turnedOn){
+
+                    String minute = (m < 10 ? "0" : "") + m;
+                    res.add(h + ":" + minute);
+                    
+                }
+            }
+        }
+
+        return res;
+        
+    }
+}
