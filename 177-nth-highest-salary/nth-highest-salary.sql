4a1fbe2239f143713e1bf85221bc5bae9defb9b1
Anurag Singh <anuragsingh260805@gmail.com>
2026-06-05 01:39:42 +0530

    Time: 433 ms (80.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    select salary from(
+        select salary , dense_rank() over(order by salary desc) as rn
+        from Employee
+    )t 
+    where rn = N
+    limit 1
+  );
+END
